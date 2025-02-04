a9e8b7507b268b44e397bd7a9cae5c025ad281a1
Talha-glitched <m.talha2703@gmail.com>
2025-02-05 01:50:52 +0500

    Set up the backend

--- crmsol/client/src/App.jsx
+++ crmsol/client/src/App.jsx
@@ -2,13 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+import './App.css';
+import Login from "./Components/Authorization/Login";
+import UserSelection from './Components/Authorization/UserSelection';
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './Components/LandingPage/LandingPage';
+import Signup from './Components/Authorization/Signup';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+  
+      <Routes>
+      <Route path="/" element={<LandingPage />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/UserSelection" element={<UserSelection />} />
+        <Route path='/Dashboard' element={<Dashboard/>} />
+      </Routes>
     </>
   )
 }
